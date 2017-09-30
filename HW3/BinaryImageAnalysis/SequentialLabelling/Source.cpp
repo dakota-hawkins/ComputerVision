01d9c74f851c71158ec231f5eaa76548280c3398
Dakota Hawkins <dakotahawkins0110@gmail.com>
2017-09-30 15:49:16 -0400

    Separated sequential labelling.

--- HW3/BinaryImageAnalysis/SequentialLabelling/Source.cpp
+++ HW3/BinaryImageAnalysis/SequentialLabelling/Source.cpp
@@ -129,6 +129,205 @@ std::vector<std::pair<int, int> > get_neighbors(cv::Mat& img, int y, int x) {
     return neighbors;
 }
 
+// Sequential labelling algorithm.
+cv::Mat sequential_label(cv::Mat& b_img){
+    // instantiate `b_img` sized grayscale image of zeros for component labels
+    cv::Mat labeled = cv::Mat::zeros(b_img.rows, b_img.cols, CV_8UC1);
+    
+    // used to keep track of labels
+    int count = 0;
+
+    // vector of sets for equivalence classes between labels.
+    std::vector<std::set<int> > set_vec;
+    
+    // Label connected regions    
+    for (int row = 0; row < b_img.rows - 1; row++) {
+    // int row = 0;
+    // while (count < 50) {
+        for (int col = 0; col < b_img.cols - 1; col++) {
+            if (b_img.at<uchar>(row + 1, col + 1) != 0) {
+                // Get lablels for neighboring pixels. 
+                int diag = labeled.at<uchar>(row, col);
+                int left = labeled.at<uchar>(row + 1, col);
+                int up = labeled.at<uchar>(row, col + 1);
+
+                // No connections, create new label.
+                if (diag == 0 && left == 0 && up == 0) {               
+                    count ++;
+                    labeled.at<uchar>(row + 1, col + 1) = count;
+                    int new_class[1] = {count};
+                    add_class(set_vec, new_class, 1);
+                    // std::cout << "Count: "  << count << std::endl;
+                    // print_equivalence_classes(set_vec);
+                }
+
+                // check if pixel connected to upper diagnol
+                if (diag != 0) {
+                    labeled.at<uchar>(row + 1, col + 1) = diag;
+                    continue;
+                } else {
+                    // check if left is labelled
+                    if (left != 0) {
+                        // check if up is labelled too
+                        if (up != 0) {
+                            // assign to left label if same
+                            if (up == left) {
+                                labeled.at<uchar>(row + 1, col + 1) = left;
+                                continue;
+                            } else {
+                                // update labels, map pixel to minimum label
+                                // map max label to minimum
+                                int small = std::min(up, left);
+                                int big = std::max(up, left);
+                                labeled.at<uchar>(row + 1, col) = small;
+                                labeled.at<uchar>(row, col + 1) = small;
+                                labeled.at<uchar>(row + 1, col + 1) = small;
+                                set_equivalencies(set_vec, small, big);
+                                continue;
+                            }
+                        // connect to left if up in not labelled
+                        } else {
+                            labeled.at<uchar>(row + 1, col + 1) = left;
+                            continue;
+                        }
+                    // check up label
+                    } else if (up != 0) {
+                        // connect pixel to up
+                        labeled.at<uchar>(row + 1, col + 1) = up;
+                        continue;
+                    }
+                }
+            }
+        }
+        // row ++;
+    }
+    std::cout << "Count: " << count << std::endl;
+    print_equivalence_classes(set_vec);
+    
+    // Map equivalent labels to each other. 
+    // only do this if count > 0
+    for (int row = 0; row < labeled.rows; row++) {
+        for (int col = 0; col < labeled.cols; col++) {
+            if (labeled.at<uchar>(row, col) != 0) {
+                labeled.at<uchar>(row, col) = get_equivalency(set_vec, labeled.at<uchar>(row, col));
+            }
+        }
+    }
+
+    return labeled;
+}
+void add_class(std::vector<std::set<int> >& class_vec, int elements[], int n) {
+    std::set<int> new_set(elements, elements + n);
+    class_vec.push_back(new_set);
+}
+
+// Generate equivalence classes.
+void set_equivalencies(std::vector<std::set<int> >& class_vec, int label1, int label2) {
+    using ::std::cout;
+    using ::std::endl;
+
+    // cout << "label1: " << label1 << endl;
+    // cout << "label2: " << label2 << endl;
+    if (class_vec.size() == 0) {
+        // cout << "Instantiating first set: {" << label1 << ", " << label2 << "}\n";
+        int new_class[2] = {label1, label2};
+        add_class(class_vec, new_class, 2);
+    } else {
+        int set1 = get_equivalency(class_vec, label1);
+        int set2 = get_equivalency(class_vec, label2);
+        // cout << "set1 index: " << set1 - 1 << endl;
+        // cout << "set1: " << endl;
+        // if (set1 - 1 > 0) {
+        //     print_set(class_vec[set1 - 1]);
+        // } else {
+        //     cout << "{}\n";
+        // }
+        
+        // cout << "set2 index: " << set2 - 1 << endl;
+        // cout << "set2: " << endl;
+        // if (set1 - 1 > 0) {
+        //     print_set(class_vec[set2 - 1]);
+        // } else {
+        //     cout << "{}\n";
+        // }
+
+        if (set1 == 0 && set2 == 0) {
+            // cout << "Neither label contained in class list\n";
+            // cout << "Adding new set." << endl;
+            int new_class[2] = {label1, label2};
+            add_class(class_vec, new_class, 2);
+            // print_equivalence_classes(class_vec);
+        } else if (set1 != 0 && set2 == 0) {
+            // cout << "Set2 not found in class list, adding to set 1 class." << endl;
+            class_vec[set1 - 1].insert(label2);
+            // print_equivalence_classes(class_vec);
+        } else if (set1 == 0 && set2 != 0) {
+            // cout << "Set1 not found in class list, adding to set 2 class." << endl;
+            class_vec[set2 - 1].insert(label1);
+            // print_equivalence_classes(class_vec);
+        } else if (set1 != set2) {
+            // cout << "Both labels already found in set list. Merging classes." << endl;
+            merge_classes(class_vec, set1 - 1, set2 - 1);
+            // print_equivalence_classes(class_vec);
+        }
+    }
+}
+
+
+void merge_classes(std::vector<std::set<int> >& class_vec,
+    int set1_idx, int set2_idx) {
+    // Implementation of removal borrowed from stack exchange.
+    // https://stackoverflow.com/questions/39912/how-do-i-remove-an-item-from-a-stl-vector-with-a-certain-value
+    
+    std::set<int>::iterator it = class_vec[set2_idx].begin();
+    std::set<int>::iterator end = class_vec[set2_idx].end();
+    for (it; it != end; it++) {
+        class_vec[set1_idx].insert(*it);
+    }
+
+    if (set2_idx != class_vec.size() - 1) {
+        std::swap(class_vec[set2_idx], class_vec.back());
+    }
+    if (class_vec.size() > 1) {
+        class_vec.pop_back();
+    }
+}
+
+
+void print_equivalence_classes(std::vector<std::set<int> >& class_vec) {
+    using ::std::cout;
+    using ::std::endl;
+    cout << "Number of sets: " << class_vec.size() << endl;
+    for (int i = 0; i < class_vec.size(); i++) {
+        print_set(class_vec[i]);
+        cout << endl;
+        // cout << "Set " << i + 1<< ":" << endl;
+        // std::set<int>::iterator start = class_vec[i].begin();
+        // std::set<int>::iterator end = class_vec[i].end();
+
+        // cout << "{";
+        // for (start; start != end; start++) {
+        //     cout << *start << ", ";
+        // }
+        // cout << "}" << endl << endl;
+    }
+}
+
+// Get equivalency class membership for element.
+int get_equivalency(std::vector<std::set<int> >& class_vec, int label) {
+    if (class_vec.size() != 0) {
+        for (int i = 0; i < class_vec.size(); i++) {
+            if (class_vec[i].find(label) != class_vec[i].end()) {
+                return i + 1;
+            }
+        }
+    }
+    return 0;
+}
+
+// cv::Mat color_labels_random(cv::Mat& label_img) {
+//     std::map<int, cv::Vec3b> color_map;
+// }
 
 
 // convert integer labels to color values. 
